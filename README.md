07feaf3595a82f70dd990d5ed0068f32a555a47b
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-12-20 01:08:55 +0100

    Project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# BidOut Auction V1 (Work in Progress)
+
+![alt text](https://github.com/kayprogrammer/bidout-auction-v1/blob/main/display.png?raw=true)
+
---
+++ bidout/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+from apps.common import views
+
+import debug_toolbar
+# handler404 = views.handler404
+# handler500 = views.handler500
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('apps.accounts.urls')),
+    path('', include('apps.shop.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
