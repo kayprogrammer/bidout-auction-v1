e394c4252a1b0ead2abfc684cdd950e23f2b1003
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-03-27 18:28:45 +0100

    managed settings properly

--- bidout/asgi.py
+++ bidout/asgi.py
@@ -6,11 +6,11 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
+from decouple import config
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bidout.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"bidout.settings.{config('SETTINGS')}")
 
 application = get_asgi_application()
--- bidout/wsgi.py
+++ bidout/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+from decouple import config
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bidout.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"bidout.settings.{config('SETTINGS')}")
 
 application = get_wsgi_application()
