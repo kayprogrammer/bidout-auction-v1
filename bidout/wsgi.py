70cbeaefe545bdc9a3f80a54c0a02a25c937a57e
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-11-26 16:35:57 +0100

    updated wsgi

--- bidout/wsgi.py
+++ bidout/wsgi.py
@@ -6,9 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
+import os, sys
+if os.environ.get('SETTINGS') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from decouple import config
-import os
 
 from django.core.wsgi import get_wsgi_application
 
