c835532e4a0a50df31ebb1d774c473f6d76beee0
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-11-26 15:53:51 +0100

    created vercel json file

--- bidout/wsgi.py
+++ bidout/wsgi.py
@@ -15,3 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"bidout.settings.{config('SETTINGS')}")
 
 application = get_wsgi_application()
+app = application
